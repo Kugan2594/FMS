a0f4c2cad0f859be9c6abe4fc43e5e446a761f16
kugan <kugan2594@gmail.com>
2022-08-31 11:02:48 +0100

    card.less file

--- src/components/organisms/Cards/CardSmall.tsx
+++ src/components/organisms/Cards/CardSmall.tsx
@@ -1,1 +1,16 @@
-export {};
+import Card from "antd/lib/card/Card";
+import React from "react";
+
+function CardSmall(props: any) {
+    return (
+        <Card className="small-card">
+            <div className="image-container">
+                <img src={props.imagesource} />
+            </div>
+
+            <div className="content-container"></div>
+        </Card>
+    );
+}
+
+export default CardSmall;
