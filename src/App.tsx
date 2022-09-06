f54ceff264ff97f5c468bae1c59cef4f81b856a2
kugan <kugan2594@gmail.com>
2022-09-06 15:38:34 +0100

    login-screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import "./App.less";
 import "antd/dist/antd.less";
 import CustomLayout from "./layout/CustomLayout";
 import { SideBar } from "organisms";
+import LoginTemplate from "./templates/LoginTemplate";
 // import { SideBar } from "organisms";
 
 function App() {
-    return <CustomLayout />;
+    return <LoginTemplate />;
 }
 
 export default App;
