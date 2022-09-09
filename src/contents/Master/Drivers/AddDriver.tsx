44cf0b7aa53643c815f8e46dff5e91a816623ed2
Kuruparan94 <kuruparanthmail94@gmail.com>
2022-09-09 12:07:20 +0530

    AddDrivers

--- src/contents/Master/Drivers/AddDriver.tsx
+++ src/contents/Master/Drivers/AddDriver.tsx
@@ -1,190 +1,142 @@
-import React from "react";
 import {
-  AutoComplete,
   Col,
+  DatePicker,
   Form,
   Input,
   Row,
   Select,
-  Typography,
+  message,
   Upload,
 } from "antd";
-import "./driver.style.less";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import type { UploadChangeParam } from "antd/es/upload";
+import type { RcFile, UploadFile, UploadProps } from "antd/es/upload/interface";
+import React, { useState } from "react";
 
-const { Option } = Select;
-
-const { Text } = Typography;
-
-const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
+const getBase64 = (img: RcFile, callback: (url: string) => void) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result as string));
+  reader.readAsDataURL(img);
 };
-const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
+
+const beforeUpload = (file: RcFile) => {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+      message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+      message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
 };
 
 function AddDriver() {
   const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
 
-  const onGenderChange = (value: string) => {
-    switch (value) {
-      case "male":
-        form.setFieldsValue({ note: "Hi, man!" });
-        return;
-      case "female":
-        form.setFieldsValue({ note: "Hi, lady!" });
-        return;
-      case "other":
-        form.setFieldsValue({ note: "Hi there!" });
-    }
+  const handleChange: UploadProps["onChange"] = (
+      info: UploadChangeParam<UploadFile>
+  ) => {
+      if (info.file.status === "uploading") {
+          setLoading(true);
+          return;
+      }
+      if (info.file.status === "done") {
+          getBase64(info.file.originFileObj as RcFile, (url) => {
+              setLoading(false);
+              setImageUrl(url);
+          });
+      }
   };
 
-  const onFinish = (values: any) => {
-    console.log(values);
-  };
+  const uploadButton = (
+      <div>
+          {loading ? <LoadingOutlined /> : <PlusOutlined />}
+          <div style={{ marginTop: 8 }}>Upload</div>
+      </div>
+  );
 
-  const onReset = () => {
-    form.resetFields();
-  };
-
-  const branch = [
-    { value: "Jaffna" },
-    { value: "Colombo" },
-    { value: "Kandy" },
-    { value: "Vavuniya" },
-  ];
+  const { Option } = Select;
 
   return (
-    <div className="add-driver">
-      <Form
-        {...layout}
-        form={form}
-        name="control-hooks"
-        onFinish={onFinish}
-        layout="vertical"
-      >
-        <Row>
-          <Col span={8}>
-            <Form.Item name="firstName" label="First Name">
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={8}>
-            <Form.Item name="lastName" label="Last Name">
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={8}>
-            <Form.Item name="nic" label="NIC Number">
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={8}>
-            <Form.Item name="email" label="Email">
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={8}>
-            <Form.Item name="contactNumber" label="Contact Number">
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={8}>
-            <Form.Item name="licenseNumber" label="License Number">
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={8}>
-            <Form.Item name="branch" label="Select Branch">
-              <AutoComplete
-                className="autoComplete"
-                options={branch}
-                placeholder="Select Branch"
-                filterOption={(inputValue, option) =>
-                  option!.value
-                    .toUpperCase()
-                    .indexOf(inputValue.toUpperCase()) !== -1
-                }
-              />
-            </Form.Item>
-          </Col>
-
-          <Col span={8}>
-            <Form.Item name="vehicle" label="Select Vehicle">
-              <AutoComplete
-                className="autoComplete"
-                options={branch}
-                placeholder="Select Vehicle"
-                filterOption={(inputValue, option) =>
-                  option!.value
-                    .toUpperCase()
-                    .indexOf(inputValue.toUpperCase()) !== -1
-                }
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-
-        <Text className="sub-title" strong>
-          User Creation
-        </Text>
-
-        <Row>
-          <Col span={8}>
-            <Form.Item name="userName" label="User Name">
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={8}>
-            <Form.Item name="password" label="Password">
-              <Input type="password" />
-            </Form.Item>
-          </Col>
-        </Row>
-        {/* <Form.Item name="note" label="Note" rules={[{ required: true }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
-        <Select
-          placeholder="Select a option and change input text above"
-          onChange={onGenderChange}
-          allowClear
-        >
-          <Option value="male">male</Option>
-          <Option value="female">female</Option>
-          <Option value="other">other</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item
-        noStyle
-        shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
-      >
-        {({ getFieldValue }) =>
-          getFieldValue('gender') === 'other' ? (
-            <Form.Item name="customizeGender" label="Customize Gender" rules={[{ required: true }]}>
-              <Input />
-            </Form.Item>
-          ) : null
-        }
-      </Form.Item>
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <Button htmlType="button" onClick={onReset}>
-          Reset
-        </Button>
-      </Form.Item> */}
-      </Form>
-    </div>
+      <>
+          <Form id="form" name="basic" form={form}>
+              <Row style={{ paddingLeft: "35px", paddingRight: "35px" }}>
+                  <Col span={24}>
+                      <Form.Item>
+                          <Select
+                              placeholder="Vehicle"
+                              optionFilterProp="children"
+                              bordered={false}
+                              style={{ borderBottom: "1px solid #ccccb3" }}
+                          >
+                              <Option value="jack">Jack</Option>
+                              <Option value="lucy">Lucy</Option>
+                              <Option value="tom">Tom</Option>
+                          </Select>
+                      </Form.Item>
+                      <Form.Item>
+                          <Input
+                              placeholder="Region"
+                              required
+                              bordered={false}
+                              style={{ borderBottom: "1px solid #ccccb3" }}
+                          />
+                      </Form.Item>
+                      <Form.Item>
+                          <DatePicker
+                              placeholder="Issued Date"
+                              style={datePickerStyle}
+                          />
+                      </Form.Item>
+                      <Form.Item>
+                          <DatePicker
+                              placeholder="Expire Date"
+                              style={datePickerStyle}
+                          />
+                      </Form.Item>
+                      <Form.Item>
+                          <Input
+                              placeholder="Price"
+                              bordered={false}
+                              required
+                              style={{ borderBottom: "1px solid #ccccb3" }}
+                          />
+                      </Form.Item>
+                      <Upload
+                          name="avatar"
+                          listType="picture-card"
+                          className="avatar-uploader"
+                          showUploadList={false}
+                          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                          beforeUpload={beforeUpload}
+                          onChange={handleChange}
+                      >
+                          {imageUrl ? (
+                              <img
+                                  src={imageUrl}
+                                  alt="avatar"
+                                  style={{ width: "100%" }}
+                              />
+                          ) : (
+                              uploadButton
+                          )}
+                      </Upload>
+                  </Col>
+              </Row>
+          </Form>
+      </>
   );
 }
 
 export default AddDriver;
+
+const datePickerStyle: React.CSSProperties = {
+  width: "100%",
+  borderLeft: "0px",
+  borderRight: "0px",
+  borderTop: "0px",
+  borderBottom: "1px solid #ccccb3",
+};
--- src/contents/Master/Drivers/ManageDrivers.tsx
+++ src/contents/Master/Drivers/ManageDrivers.tsx
@@ -2,112 +2,270 @@ import { Button, Modal } from "antd";
 import React, { useState } from "react";
 import MasterTemplateWithSmallCard from "../../../templates/MasterTemplateWithSmallCard";
 import AddDriver from "./AddDriver";
-import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+import { RocketOutlined } from "@ant-design/icons";
 
 const { confirm } = Modal;
 
 function ManageDrivers() {
-
-    const [showAddModel, setShowAddModel] = useState(false);
-    const [showUpdateModel, setShowUpdateModel] = useState(false);
-    const [updateData, setUpdateData] = useState({id: "",
+  const [updateData, setUpdateData] = useState({
+    id: "",
     progressData: 0,
     itemName: "",
     branchLocation: "",
-    branchName: "",});
+    branchName: "",
+  });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isEdit, setisEdit] = useState(false);
+    const [eco,setEco]=useState([]);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+        setisEdit(false);
+    };
+
+    const showModalEdit = () => {
+        setIsModalOpen(true);
+        setisEdit(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
 
     const handleCancel = () => {
-
+        setIsModalOpen(false);
     };
 
-    const handleAdd = () => {
+  const deleteClickHandler = (id: any) => {
+    confirm({
+      title: "Are you sure delete this Driver?",
+      icon: <ExclamationCircleOutlined />,
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+      onOk() {
+        console.log(id);
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  };
 
-    };
-
-    const updateClickHandler = (data:any) => {
-        setShowUpdateModel(true);
-        setUpdateData(data);
-    };
-
-    const deleteClickHandler = (id:any) => {
-      confirm({
-        title: 'Are you sure delete this Driver?',
-        icon: <ExclamationCircleOutlined />,
-        okText: 'Yes',
-        okType: 'danger',
-        cancelText: 'No',
-        onOk() {
-          console.log(id);
-        },
-        onCancel() {
-          console.log('Cancel');
-        },
-      });
-    };
-    
-  const data = [{
-            id: "1 one",
-            progressData: 49,
-            itemName: "Colombo",
-            branchLocation: "NP CAR 5625",
-            branchName: "Cudeson",
-        },
-        {
-            id: "2 two",
-            progressData: 49,
-            itemName: "Colombo",
-            branchLocation: "NP CAR 5625",
-            branchName: "Kuganesan",
-        },];
+  const data = [
+    {
+      id: "1",
+      numberOfVehicles: 34,
+      progressData: 49,
+      itemName: "Vehicles",
+      branchLocation: "Colombo",
+      name: "Colombo Branch",
+      adminName: "Michael Clarke",
+      contactNumber: "0772250114",
+      vehicleNumber: "MJ2200",
+      vehicleType: "car",
+      designation: "manager",
+      drivingLicense: "82763871",
+      vehicleModel: "Tyota MT",
+      generatorBrand: "Honda",
+      fuelType: "gasoline",
+      nic: "941234500V",
+      generatorName: "generator-A",
+      driverName: "michael Clarke",
+      vehicleIcon: <RocketOutlined />,
+    },
+    {
+      id: "1",
+      numberOfVehicles: 34,
+      progressData: 49,
+      itemName: "Vehicles",
+      branchLocation: "Colombo",
+      name: "Colombo Branch",
+      adminName: "Michael Clarke",
+      contactNumber: "0772250114",
+      vehicleNumber: "MJ2200",
+      vehicleType: "car",
+      designation: "manager",
+      drivingLicense: "82763871",
+      vehicleModel: "Tyota MT",
+      generatorBrand: "Honda",
+      fuelType: "gasoline",
+      nic: "941234500V",
+      generatorName: "generator-A",
+      driverName: "michael Clarke",
+      vehicleIcon: <RocketOutlined />,
+    },
+    {
+      id: "1",
+      numberOfVehicles: 34,
+      progressData: 49,
+      itemName: "Vehicles",
+      branchLocation: "Colombo",
+      name: "Colombo Branch",
+      adminName: "Michael Clarke",
+      contactNumber: "0772250114",
+      vehicleNumber: "MJ2200",
+      vehicleType: "car",
+      designation: "manager",
+      drivingLicense: "82763871",
+      vehicleModel: "Tyota MT",
+      generatorBrand: "Honda",
+      fuelType: "gasoline",
+      nic: "941234500V",
+      generatorName: "generator-A",
+      driverName: "michael Clarke",
+      vehicleIcon: <RocketOutlined />,
+    },
+    {
+      id: "1",
+      numberOfVehicles: 34,
+      progressData: 49,
+      itemName: "Vehicles",
+      branchLocation: "Colombo",
+      name: "Colombo Branch",
+      adminName: "Michael Clarke",
+      contactNumber: "0772250114",
+      vehicleNumber: "MJ2200",
+      vehicleType: "car",
+      designation: "manager",
+      drivingLicense: "82763871",
+      vehicleModel: "Tyota MT",
+      generatorBrand: "Honda",
+      fuelType: "gasoline",
+      nic: "941234500V",
+      generatorName: "generator-A",
+      driverName: "michael Clarke",
+      vehicleIcon: <RocketOutlined />,
+    },
+    {
+      id: "1",
+      numberOfVehicles: 34,
+      progressData: 49,
+      itemName: "Vehicles",
+      branchLocation: "Colombo",
+      name: "Colombo Branch",
+      adminName: "Michael Clarke",
+      contactNumber: "0772250114",
+      vehicleNumber: "MJ2200",
+      vehicleType: "car",
+      designation: "manager",
+      drivingLicense: "82763871",
+      vehicleModel: "Tyota MT",
+      generatorBrand: "Honda",
+      fuelType: "gasoline",
+      nic: "941234500V",
+      generatorName: "generator-A",
+      driverName: "michael Clarke",
+      vehicleIcon: <RocketOutlined />,
+    },
+    {
+      id: "1",
+      numberOfVehicles: 34,
+      progressData: 49,
+      itemName: "Vehicles",
+      branchLocation: "Colombo",
+      name: "Colombo Branch",
+      adminName: "Michael Clarke",
+      contactNumber: "0772250114",
+      vehicleNumber: "MJ2200",
+      vehicleType: "car",
+      designation: "manager",
+      drivingLicense: "82763871",
+      vehicleModel: "Tyota MT",
+      generatorBrand: "Honda",
+      fuelType: "gasoline",
+      nic: "941234500V",
+      generatorName: "generator-A",
+      driverName: "michael Clarke",
+      vehicleIcon: <RocketOutlined />,
+    },
+    {
+      id: "1",
+      numberOfVehicles: 34,
+      progressData: 49,
+      itemName: "Vehicles",
+      branchLocation: "Colombo",
+      name: "Colombo Branch",
+      adminName: "Michael Clarke",
+      contactNumber: "0772250114",
+      vehicleNumber: "MJ2200",
+      vehicleType: "car",
+      designation: "manager",
+      drivingLicense: "82763871",
+      vehicleModel: "Tyota MT",
+      generatorBrand: "Honda",
+      fuelType: "gasoline",
+      nic: "941234500V",
+      generatorName: "generator-A",
+      driverName: "michael Clarke",
+      vehicleIcon: <RocketOutlined />,
+    },
+    {
+      id: "1",
+      numberOfVehicles: 34,
+      progressData: 49,
+      itemName: "Vehicles",
+      branchLocation: "Colombo",
+      name: "Colombo Branch",
+      adminName: "Michael Clarke",
+      contactNumber: "0772250114",
+      vehicleNumber: "MJ2200",
+      vehicleType: "car",
+      designation: "manager",
+      drivingLicense: "82763871",
+      vehicleModel: "Tyota MT",
+      generatorBrand: "Honda",
+      fuelType: "gasoline",
+      nic: "941234500V",
+      generatorName: "generator-A",
+      driverName: "michael Clarke",
+      vehicleIcon: <RocketOutlined />,
+    },
+    {
+      id: "1",
+      numberOfVehicles: 34,
+      progressData: 49,
+      itemName: "Vehicles",
+      branchLocation: "Colombo",
+      name: "Colombo Branch",
+      adminName: "Michael Clarke",
+      contactNumber: "0772250114",
+      vehicleNumber: "MJ2200",
+      vehicleType: "car",
+      designation: "manager",
+      drivingLicense: "82763871",
+      vehicleModel: "Tyota MT",
+      generatorBrand: "Honda",
+      fuelType: "gasoline",
+      nic: "941234500V",
+      generatorName: "generator-A",
+      driverName: "michael Clarke",
+      vehicleIcon: <RocketOutlined />,
+    },
+  ];
 
   return (
     <div>
       <MasterTemplateWithSmallCard
         data={data}
         dataCount={data.length}
-        headerOnClickAdd={() => setShowAddModel(true)}
+        headerOnClickAdd={showModal}
         headerOnSearch={() => {}}
         cardOnClick={() => {}}
-        onClickDelete={(id:any) => deleteClickHandler(id)}
-        onClickUpdate={(data:any) => updateClickHandler(data)}
+        onClickDelete={(id: any) => deleteClickHandler(id)}
+        onClickUpdate={showModalEdit}
+        driverCard={true}
       />
       <Modal
-        visible={showAddModel}
-        title="Add New Driver"
-        width="75%"
-        onCancel={() => setShowAddModel(false)}
-        footer={[
-          <Button onClick={() => setShowAddModel(false)}>
-            Cancel
-          </Button>,
-          <Button
-            type="primary"
-            onClick={handleAdd}
-          >
-            Add
-          </Button>,
-        ]}
+        title={isEdit ? "Edit Driver" : "Add New Driver"}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        closable={false}
+        width={500}
       >
-        {<AddDriver />}
-      </Modal>
-      <Modal
-        visible={showUpdateModel}
-        title="Update Driver"
-        width="50%"
-        onCancel={() => setShowUpdateModel(false)}
-        footer={[
-          <Button onClick={() => setShowUpdateModel(false)}>
-            Cancel
-          </Button>,
-          <Button
-            type="primary"
-            onClick={handleAdd}
-          >
-            Update
-          </Button>,
-        ]}
-      >
-        {updateData.branchName}
+        <AddDriver />
       </Modal>
     </div>
   );
