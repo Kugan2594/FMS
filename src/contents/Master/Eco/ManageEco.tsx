a80f4bea3f5c38388594b0ee1da6c7aef78a2999
Rishi004 <rishireiko@gmail.com>
2022-09-12 10:24:59 +0530

    add / edit generator screen design

--- src/contents/Master/Eco/ManageEco.tsx
+++ src/contents/Master/Eco/ManageEco.tsx
@@ -4,29 +4,27 @@ import { useEffect, useState } from "react";
 import AddEco from "./AddEco";
 import MasterTemplateWithLargeCard from "../../../templates/MasterTemplateWithLargeCard";
 import { createDeflateRaw } from "zlib";
-import {
-    getAllEmissionTestDocumentByUserId
-  } from "./ServicesEco";
-  import { getUserDetails} from "../../Login/LoginAuthentication";
-function createData(data:any){
-    let convertData=data.map((post:any,index:any)=>{
-return {
-    id: post.id,
-    name: "EmissionTest",
-    progressData: post.emissionTestValidity,
-    vehicleNo: post.vehicleResponseDto.vehicleOwner,
-    vehicleModel: post.vehicleResponseDto.resourceVehicleDto.vehicleModel
-    ,
-    branchName: "Jaffna Branch",
-    dueDate: post.emissionTestExpiryDate,
-}
-    })
-    return convertData
+import { getAllEmissionTestDocumentByUserId } from "./ServicesEco";
+import { getUserDetails } from "../../Login/LoginAuthentication";
+function createData(data: any) {
+    let convertData = data.map((post: any, index: any) => {
+        return {
+            id: post.id,
+            name: "EmissionTest",
+            progressData: post.emissionTestValidity,
+            vehicleNo: post.vehicleResponseDto.vehicleOwner,
+            vehicleModel:
+                post.vehicleResponseDto.resourceVehicleDto.vehicleModel,
+            branchName: "Jaffna Branch",
+            dueDate: post.emissionTestExpiryDate,
+        };
+    });
+    return convertData;
 }
 function ManageEco() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isEdit, setisEdit] = useState(false);
-    const [eco,setEco]=useState([]);
+    const [eco, setEco] = useState([]);
     const showModal = () => {
         setIsModalOpen(true);
         setisEdit(false);
@@ -45,21 +43,22 @@ function ManageEco() {
         setIsModalOpen(false);
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         getAllEmissionTestData(getUserDetails().user_id);
-    }, [])
-    const getAllEmissionTestData=(userId: number)=>{
-        let data: any=[];
+    }, []);
+    const getAllEmissionTestData = (userId: number) => {
+        let data: any = [];
 
         getAllEmissionTestDocumentByUserId(userId).then(
-            (res:any) => {
-                data=createData(res.results.emissionTest);
+            (res: any) => {
+                data = createData(res.results.emissionTest);
                 setEco(data);
-            },(error:any)=>{
+            },
+            (error: any) => {
                 setEco([]);
             }
-        )
-    }
+        );
+    };
     return (
         <>
             <MasterTemplateWithLargeCard
