80c9833ba18f1be6583451cd3cd267e39007b3e4
Kuruparan94 <cude1995son@gmail.com>
2022-09-20 16:32:12 +0530

    Branch module integration

--- src/contents/Master/Branch/AddBranch.tsx
+++ src/contents/Master/Branch/AddBranch.tsx
@@ -1,135 +1,177 @@
-import { Col, Form, Image, message, Modal, Row, Upload } from "antd";
+import { Col, Form, Image, message, Modal, Row, Upload, Button } from "antd";
 import { Input } from "../../../components/atoms/index";
 import Column from "antd/lib/table/Column";
-import { Button } from "../../../components/atoms/Button/index";
 import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
 import type { UploadChangeParam } from "antd/es/upload";
 import type { RcFile, UploadFile, UploadProps } from "antd/es/upload/interface";
+import { getUserDetails } from "../../../contents/Login/LoginAuthentication";
 import React, { useState } from "react";
+import { errHandler, branchAddSuccess } from "../../../helper/helper";
+import { addBranch, editBranch } from "./ServicesBranch";
 interface AddBranchType {
-    onClickCancel?: React.MouseEventHandler<HTMLElement> | undefined;
-    onClickAdd?: React.MouseEventHandler<HTMLElement> | undefined;
-    initialValues?: any;
+  onClickCancel?: React.MouseEventHandler<HTMLElement> | undefined;
+  onClickAdd?: React.MouseEventHandler<HTMLElement> | undefined;
+  initialValues?: any;
 }
 const getBase64 = (img: RcFile, callback: (url: string) => void) => {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => callback(reader.result as string));
-    reader.readAsDataURL(img);
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result as string));
+  reader.readAsDataURL(img);
 };
 
 const beforeUpload = (file: RcFile) => {
-    const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-    if (!isJpgOrPng) {
-        message.error("You can only upload JPG/PNG file!");
-    }
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    if (!isLt2M) {
-        message.error("Image must smaller than 2MB!");
-    }
-    return isJpgOrPng && isLt2M;
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
 };
 
-function AddBranch({
-    onClickAdd,
-    onClickCancel,
-    initialValues,
-}: AddBranchType) {
-    const [form] = Form.useForm();
-    const [loading, setLoading] = useState(false);
-    const [imageUrl, setImageUrl] = useState<string>();
-    const uploadButton = (
-        <div>
-            {loading ? <LoadingOutlined /> : <PlusOutlined />}
-            <div style={{ marginTop: 8 }}>Upload</div>
-        </div>
-    );
+function AddBranch(props: any) {
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
 
-    const handleChange: UploadProps["onChange"] = (
-        info: UploadChangeParam<UploadFile>
-    ) => {
-        if (info.file.status === "uploading") {
-            setLoading(true);
-            return;
-        }
-        if (info.file.status === "done") {
-            getBase64(info.file.originFileObj as RcFile, (url) => {
-                setLoading(false);
-                setImageUrl(url);
-            });
-        }
-    };
-    return (
-        <Form initialValues={initialValues}>
-            <Row gutter={8}>
-                <Col xs={24} xl={6} style={{ marginLeft: "0px" }}>
-                    <Row className="row-1">
-                        <Form.Item valuePropName="fileList">
-                            <Upload
-                                name="avatar"
-                                listType="picture-card"
-                                className="avatar-uploader"
-                                showUploadList={false}
-                                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                                beforeUpload={beforeUpload}
-                                onChange={handleChange}
-                            >
-                                {imageUrl ? (
-                                    <img
-                                        src={imageUrl}
-                                        alt="avatar"
-                                        style={{ width: "100%" }}
-                                    />
-                                ) : (
-                                    uploadButton
-                                )}
-                            </Upload>
-                        </Form.Item>
-                    </Row>
-                    <Row className="row-2"></Row>
-                    <Row className="row-3"></Row>
-                </Col>
-                <Col xs={24} xl={18}>
-                    <div
-                        className="input-container"
-                        style={{
-                            width: "100%",
-                            marginTop: "0px",
-                        }}
-                    >
-                        <Form.Item name="branchName">
-                            <Input label="branchName" />
-                        </Form.Item>
-                        <Form.Item name="branchLocation">
-                            <Input label="Address" />
-                        </Form.Item>
-                        <Form.Item name="contactNumber">
-                            <Input label="Contact Number" />
-                        </Form.Item>
-                        <Form.Item name="email">
-                            <Input label="email" />
-                        </Form.Item>
-                    </div>
-                </Col>
-            </Row>
+  const handleChange: UploadProps["onChange"] = (
+    info: UploadChangeParam<UploadFile>
+  ) => {
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      getBase64(info.file.originFileObj as RcFile, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
 
-            <Row
-                justify="end"
-                className="Actions"
-                gutter={8}
-                style={{
-                    marginTop: "3%",
-                }}
-            >
-                <Col>
-                    {" "}
-                    <Button title="Cancel" onClick={onClickCancel} />
-                </Col>
-                <Col>
-                    <Button title="Add" type="primary" onClick={onClickAdd} />
-                </Col>
-            </Row>
-        </Form>
-    );
+  const onFinishFailed = () => {};
+
+  const onFinishAdd = (values: any) => {
+    let branchData = values;
+    if (props.action == "add") {
+      const data = {
+        id: 100,
+        branchName: branchData.branchName,
+        address: branchData.address,
+        phoneNumber: branchData.phoneNumber,
+        email: branchData.email,
+        companyId: getUserDetails().company_id,
+        //   companyId:  1
+      };
+
+      addBranch(data)
+        .then((res: any) => {
+          branchAddSuccess();
+          props.reloadTable(res);
+        })
+        .catch((err: any) => {
+          errHandler(err);
+        });
+    } else {
+      const data = {
+        id: props.initialValues.id,
+        branchName: branchData.branchName,
+        address: branchData.address,
+        phoneNumber: branchData.phoneNumber,
+        email: branchData.email,
+        //   companyId:  getUserDetails().company_id
+        companyId: 1,
+      };
+
+      editBranch(data)
+        .then((res: any) => {
+          branchAddSuccess();
+          props.reloadTable(res);
+        })
+        .catch((err: any) => {
+          errHandler(err);
+        });
+    }
+  };
+  return (
+    <Form
+      id="form"
+      name="basic"
+      form={form}
+      initialValues={props.isEdit ? props.updateData : {}}
+      onFinish={onFinishAdd}
+      onFinishFailed={onFinishFailed}
+    >
+      <Row gutter={8}>
+        <Col xs={24} xl={6} style={{ marginLeft: "0px" }}>
+          <Row className="row-1">
+            <Form.Item valuePropName="fileList">
+              <Upload
+                name="avatar"
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList={false}
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                beforeUpload={beforeUpload}
+                onChange={handleChange}
+              >
+                {imageUrl ? (
+                  <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+                ) : (
+                  uploadButton
+                )}
+              </Upload>
+            </Form.Item>
+          </Row>
+          <Row className="row-2"></Row>
+          <Row className="row-3"></Row>
+        </Col>
+        <Col xs={24} xl={18}>
+          <div
+            className="input-container"
+            style={{
+              width: "100%",
+              marginTop: "0px",
+            }}
+          >
+            <Form.Item name="branchName">
+              <Input label="branchName" />
+            </Form.Item>
+            <Form.Item name="address">
+              <Input label="Address" />
+            </Form.Item>
+            <Form.Item name="phoneNumber">
+              <Input label="Contact Number" />
+            </Form.Item>
+            <Form.Item name="email">
+              <Input label="email" />
+            </Form.Item>
+          </div>
+        </Col>
+      </Row>
+
+      <Row
+        justify="end"
+        className="Actions"
+        gutter={8}
+        style={{
+          marginTop: "3%",
+        }}
+      >
+        <Button htmlType="submit" type="primary">
+          Add
+        </Button>
+      </Row>
+    </Form>
+  );
 }
 
 export default AddBranch;
--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -1,128 +1,148 @@
 import { notification } from "antd";
 
 export const errHandler = (err: any) => {
-    console.log(err);
+  console.log(err);
 
-    let error;
+  let error;
 
-    if (err.status === "VALIDATION_FAILURE") {
-        error = err.validationFailures[0];
+  if (err.status === "VALIDATION_FAILURE") {
+    error = err.validationFailures[0];
+  }
+  let { code } = error;
+
+  const errorBox = (errCode: any, errMessage: any) => {
+    if (code === errCode) {
+      return notification.error({
+        message: errMessage,
+        duration: 3,
+      });
     }
-    let { code } = error;
+  };
 
-    const errorBox = (errCode: any, errMessage: any) => {
-        if (code === errCode) {
-            return notification.error({
-                message: errMessage,
-                duration: 3,
-            });
-        }
-    };
+  // Common
+  errorBox("120000", "Notify Account Balance Not Enough");
 
-    // Common
-    errorBox("120000", "Notify Account Balance Not Enough");
+  // Vehicle
+  errorBox("1306", "Vehicle Number Already Exists");
 
-    // Vehicle
-    errorBox("1306", "Vehicle Number Already Exists");
+  // Insurance Document
+  errorBox("12201", "Insurance Document already exist for this Vehicle");
 
-    // Insurance Document
-    errorBox("12201", "Insurance Document already exist for this Vehicle");
+  // Revenue License
+  errorBox("12001", "Revenue License Already Exist for this Vehicle");
 
-    // Revenue License
-    errorBox("12001", "Revenue License Already Exist for this Vehicle");
+  // Emission Test
+  errorBox("12101", "Emission Test Already Exist for this Vehicle");
 
-    // Emission Test
-    errorBox("12101", "Emission Test Already Exist for this Vehicle");
+  // Driver
+  errorBox("100016", "Email Already Exists");
+  errorBox("13025", "Driving License No Already Exists");
+  errorBox("100019", "Mobile No Already Exists");
+  errorBox("100020", "Nic No Already Exists");
 
-    // Driver
-    errorBox("100016", "Email Already Exists");
-    errorBox("13025", "Driving License No Already Exists");
-    errorBox("100019", "Mobile No Already Exists");
-    errorBox("100020", "Nic No Already Exists");
-
+  // Branch
+  errorBox("1001", "Branch is depended");
 };
 
 //success message popups
 export const insuranceDocumentAddSuccess = () =>
-    notification.success({
-        message: "Insurance Document Added Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Insurance Document Added Successfully",
+    duration: 3,
+  });
 
 export const revenueLicenseDocumentAddSuccess = () =>
-    notification.success({
-        message: "Revenue License Document Added Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Revenue License Document Added Successfully",
+    duration: 3,
+  });
 
 export const emissionTestDocumentAddSuccess = () =>
-    notification.success({
-        message: "Emission Test Document Added Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Emission Test Document Added Successfully",
+    duration: 3,
+  });
 
 export const loginSuccess = () =>
-    notification.success({
-        message: "Successfully logged in",
-        duration: 3,
-    });
+  notification.success({
+    message: "Successfully logged in",
+    duration: 3,
+  });
 
 export const loginError = () =>
-    notification.error({
-        message: "Incorrect username or password",
-        duration: 3,
-    });
+  notification.error({
+    message: "Incorrect username or password",
+    duration: 3,
+  });
 
 export const Notification = (message: any) => {
-    return notification.success({
-        message: message,
-    });
+  return notification.success({
+    message: message,
+  });
 };
 
 export const generatorAddSuccess = () =>
-    notification.success({
-        message: "Generator Added Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Generator Added Successfully",
+    duration: 3,
+  });
 
 export const generatorUpdateSuccess = () =>
-    notification.success({
-        message: "Generator Updated Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Generator Updated Successfully",
+    duration: 3,
+  });
 
 export const generatorDeleteSuccess = () =>
-    notification.success({
-        message: "Generator Deleted Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Generator Deleted Successfully",
+    duration: 3,
+  });
 
 export const vehicleAddSuccess = () =>
-    notification.success({
-        message: "Vehicle Added Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Vehicle Added Successfully",
+    duration: 3,
+  });
 
 export const fuelUpAddSuccess = () =>
-    notification.success({
-        message: "Fuel Up Added Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Fuel Up Added Successfully",
+    duration: 3,
+  });
 
 export const mileageAddSuccess = () =>
-    notification.success({
-        message: "Mileage Added Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Mileage Added Successfully",
+    duration: 3,
+  });
 
 export const driverAddSuccess = () =>
-    notification.success({
-        message: "Driver Added Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Driver Added Successfully",
+    duration: 3,
+  });
 
 export const driverDeleteSuccess = () =>
-    notification.success({
-        message: "Driver Deleted Successfully",
-        duration: 3,
-    });
+  notification.success({
+    message: "Driver Deleted Successfully",
+    duration: 3,
+  });
+
+export const branchAddSuccess = () =>
+  notification.success({
+    message: "Branch Added Successfully",
+    duration: 3,
+  });
+
+export const branchUpdateSuccess = () =>
+  notification.success({
+    message: "Branch Updated Successfully",
+    duration: 3,
+  });
+
+export const branchDeleteSuccess = () =>
+  notification.success({
+    message: "Branch Deleted Successfully",
+    duration: 3,
+  });
