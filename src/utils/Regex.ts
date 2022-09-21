13fcc5e2eee5cd54e9dc2ee4e8e339002b46e6ca
Kuruparan94 <cude1995son@gmail.com>
2022-09-21 16:16:27 +0530

    page reload for revenue license document

--- src/utils/Regex.ts
+++ src/utils/Regex.ts
@@ -1,10 +1,15 @@
 export const noSplCharAndLetterRegex = /^[0-9]{2,10}$/;
 
-export const phoneNumberRegex =
-    /^(94)?(?:(7)(0|1|2|3|4|5|6|7|8|9)\d)\d{6}$/; 
-    
+export const phoneNumberRegex = /^(94)?(?:(7)(0|1|2|3|4|5|6|7|8|9)\d)\d{6}$/;
+
 export const noSplCharAndNoRegex = /^[a-zA-Z\s]*$/;
 
 export const nicNoRegex = /^([0-9]{9}[x|X|v|V]|[0-9]{12})$/;
 
-export const drivingLicenseNoRegex = /^([A-Z]{1}[0-9]{7}[A-Z]{1})|([A-Z]{1}[0-9]{7})$/;
+export const drivingLicenseNoRegex =
+  /^([A-Z]{1}[0-9]{7}[A-Z]{1})|([A-Z]{1}[0-9]{7})$/;
+
+export const contactNoRegex =
+  /^(?:0|0094|94)?(?:(7)|2|(0|1|2|3|4|5|6|7|8|9)\d)\d{7}$/;
+
+export const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
